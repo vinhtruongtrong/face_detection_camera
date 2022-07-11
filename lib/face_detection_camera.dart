f42042dcc6cd5546b6c1a2296ce1d8b4359c7587
Vinh Truong Trong <vinh.truongtrong@gmail.com>
2022-07-11 10:23:25 +0700

    make it better

--- lib/face_detection_camera.dart
+++ lib/face_detection_camera.dart
@@ -4,3 +4,5 @@ export 'package:face_detection_camera/src/widgets/view.dart';
 export 'package:face_detection_camera/src/models/detected_face.dart';
 export 'package:face_detection_camera/src/utils/utils.dart';
 export 'package:face_detection_camera/src/res/results.dart';
+export 'package:face_detection_camera/src/res/enums.dart';
+export 'package:face_detection_camera/src/res/extensions.dart';
